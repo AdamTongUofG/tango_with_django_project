c62ae04a90a12829b85add271a9dd78ce786f4f8
Adam Tong <2733692t@student.gla.ac.uk>
2024-02-04 18:50:04 +0000

    Rango App Completed, amended for chapter 4 tests

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'rango'
 
@@ -14,4 +16,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('restricted/', views.restricted, name='restricted'),
     path('logout/', views.user_logout, name='logout'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
